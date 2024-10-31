7f4ed4f14728c2de591af723ba37b6ffdfd361c7
Fareen Kashmira <fareenkashmira@gmail.com>
2024-10-31 18:41:20 +0530

    Create privacypolicy.js

---
+++ js/privacypolicy.js
@@ -0,0 +1,7 @@
+// script.js
+
+// Add any JavaScript functionality here in the future.
+// For example, you could add functionality to dynamically handle form submissions,
+// or enhance the user experience with animations or event listeners.
+
+console.log("Kooky Privacy Policy JS loaded."); // Just a placeholder to indicate the script is running.
