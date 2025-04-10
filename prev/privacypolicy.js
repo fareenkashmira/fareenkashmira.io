25cac94dc9533ea9aa768d412b5b850f0f1e2105
Fareen Kashmira <fareenkashmira@gmail.com>
2025-04-10 12:43:10 +0530

    Delete prev directory

--- prev/privacypolicy.js
+++
@@ -1,7 +0,0 @@
-// script.js
-
-// Add any JavaScript functionality here in the future.
-// For example, you could add functionality to dynamically handle form submissions,
-// or enhance the user experience with animations or event listeners.
-
-console.log("Kooky Privacy Policy JS loaded."); // Just a placeholder to indicate the script is running.
